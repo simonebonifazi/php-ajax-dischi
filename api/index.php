390e0113e40a5df3cd53368baef2f61fa75b695c
simonebonifazi <simonbonifraz@gmail.com>
2022-09-06 01:25:26 +0200

    added and checked api

--- api/index.php
+++ api/index.php
@@ -1,1 +1,11 @@
 <!-- qui sviluppo la chiamata api  -->
+
+<?php 
+require '../includes/discs.php';
+
+// var_dump($discs);
+header('Content-type: applicaation/json');
+
+echo json_encode($discs);
+
+?>
